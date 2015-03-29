f12ab1889a6a5234cdebcfb660ec82995dc5f095
Cristóbal Valverde Albo <cristobal@toomanymelons.com>
2015-03-29 20:38:56 +0200

    Cambiado el código que genera constantes en Java de enumeraciones a enteros.

--- lib/database-mysql/Constants.java
+++ lib/database-mysql/Constants.java
@@ -1,129 +1,69 @@
 public class Constants{
 
-	public enum RouteStatus{
-		UNASSIGNED(1),
-		ROUTE_TO_BE_DEFINED(2),
-		READY(3),
-		IN_COURSE(4),
-		FINISHED(5),
-		WITH_INCIDENTS(6),
-		PERMANENT(7);
+	public class RouteStatus{
+		public static final int UNASSIGNED = 1;
+		public static final int ROUTE_TO_BE_DEFINED = 2;
+		public static final int READY = 3;
+		public static final int IN_COURSE = 4;
+		public static final int FINISHED = 5;
+		public static final int WITH_INCIDENTS = 6;
+		public static final int PERMANENT = 7;
+	}
 
-		int value;
+	public class ParcelStatus{
+		public static final int UNASSIGNED = 0;
+		public static final int IN_DESTINATION_WAREHOUSE = 1;
+		public static final int WITH_INCIDENTS = 2;
+		public static final int DELIVERED = 3;
+		public static final int ABSENT_OR_CLOSED = 4;
+		public static final int WRONG_ADRESS = 5;
+		public static final int INCOMPLETE_ADRESS = 6;
+		public static final int TO_BE_COLLECTED = 7;
+		public static final int COLLECTED = 8;
+		public static final int IN_WAREHOUSE_PICK_UP = 9;
+		public static final int ON_ROUTE = 10;
+		public static final int FAILED_CASH_ON_DELIVERY = 11;
+		public static final int TO_RETURN_TO_SENDER = 13;
+		public static final int DELIVERY_POSTPONED_BY_CUSTOMER = 14;
+		public static final int RETURNED_TO_SENDER = 15;
+		public static final int ORIGIN_NOT_READY = 16;
+		public static final int IN_ORIGIN_WAREHOUSE = 17;
+		public static final int DEFINITELY_LOST = 18;
+		public static final int IN_TRANSIT = 19;
+		public static final int UNBOUND = 20;
+	}
 
-		public RouteStatus(int value){
-			this.value = value;
-		}
+	public class DestinationTypes{
+		public static final int UNASSIGNED = 0;
+		public static final int HOUSEHOLD = 1;
+		public static final int OFFICE = 2;
+		public static final int PREMISES = 3;
+	}
 
-		public int getValue(){
-			return value;
-		}
-	};
+	public class ServiceTypes{
+		public static final int UNASSIGNED = 0;
+		public static final int NEXT_DAY = 1;
+		public static final int SAME-DAY = 2;
+		public static final int RETURN = 3;
+		public static final int DIRECT = 4;
+		public static final int INTERNATIONAL = 5;
+		public static final int CORREO_POSTAL = 6;
+		public static final int CORREOS_EXPRESS = 7;
+	}
 
-	public enum ParcelStatus{
-		UNASSIGNED(0),
-		IN_DESTINATION_WAREHOUSE(1),
-		WITH_INCIDENTS(2),
-		DELIVERED(3),
-		ABSENT_OR_CLOSED(4),
-		WRONG_ADRESS(5),
-		INCOMPLETE_ADRESS(6),
-		TO_BE_COLLECTED(7),
-		COLLECTED(8),
-		IN_WAREHOUSE_PICK_UP(9),
-		ON_ROUTE(10),
-		FAILED_CASH_ON_DELIVERY(11),
-		TO_RETURN_TO_SENDER(13),
-		DELIVERY_POSTPONED_BY_CUSTOMER(14),
-		RETURNED_TO_SENDER(15),
-		ORIGIN_NOT_READY(16),
-		IN_ORIGIN_WAREHOUSE(17),
-		DEFINITELY_LOST(18),
-		IN_TRANSIT(19),
-		UNBOUND(20);
+	public class DemandTypes{
+		public static final int REGULAR = 1;
+		public static final int GROUPED = 2;
+		public static final int RETURN = 3;
+	}
 
-		int value;
-
-		public ParcelStatus(int value){
-			this.value = value;
-		}
-
-		public int getValue(){
-			return value;
-		}
-	};
-
-	public enum DestinationTypes{
-		UNASSIGNED(0),
-		HOUSEHOLD(1),
-		OFFICE(2),
-		PREMISES(3);
-
-		int value;
-
-		public DestinationTypes(int value){
-			this.value = value;
-		}
-
-		public int getValue(){
-			return value;
-		}
-	};
-
-	public enum ServiceTypes{
-		UNASSIGNED(0),
-		NEXT_DAY(1),
-		SAME-DAY(2),
-		RETURN(3),
-		DIRECT(4),
-		INTERNATIONAL(5),
-		CORREO_POSTAL(6),
-		CORREOS_EXPRESS(7);
-
-		int value;
-
-		public ServiceTypes(int value){
-			this.value = value;
-		}
-
-		public int getValue(){
-			return value;
-		}
-	};
-
-	public enum DemandTypes{
-		REGULAR(1),
-		GROUPED(2),
-		RETURN(3);
-
-		int value;
-
-		public DemandTypes(int value){
-			this.value = value;
-		}
-
-		public int getValue(){
-			return value;
-		}
-	};
-
-	public enum StreetTypes{
-		UNASSIGNED(0),
-		CALLE(1),
-		AVENIDA(2),
-		PASEO(3),
-		PLAZA(4),
-		PASAJE(5);
-
-		int value;
-
-		public StreetTypes(int value){
-			this.value = value;
-		}
-
-		public int getValue(){
-			return value;
-		}
-	};
+	public class StreetTypes{
+		public static final int UNASSIGNED = 0;
+		public static final int CALLE = 1;
+		public static final int AVENIDA = 2;
+		public static final int PASEO = 3;
+		public static final int PLAZA = 4;
+		public static final int PASAJE = 5;
+	}
 
 }
